6ec75fbc02fe29e975bc32c927f3a51279270d3e
Ognjen Babovic <obabovic@MEGAZORD.local>
2016-12-23 18:16:23 +0100

    Fix parser bugs according to new input test

--- src/utils/ParserActionImplementer.java
+++ src/utils/ParserActionImplementer.java
@@ -5,139 +5,69 @@ import rs.etf.pp1.symboltable.Tab;
 import rs.etf.pp1.symboltable.concepts.*;
 
 import java.lang.*;
+import java.util.EnumMap;
+import java.util.Map;
+import org.apache.log4j.Logger;
+
+
 
 public class ParserActionImplementer {	
-	
-
-	public void report_error(String msg, int line) {
-
-	}
-
-	public void report_info(String msg, int line) {
-
-	}
-
-	public void report_error(String msg) {
-
-	}
-
-	public void report_info(String msg) {
-
-	}
-
-	public void startProgram(String name) {
-
-	}
-
-	public void endProgram() {
-
-	}
-
-	public Struct findType(String typeName, int line) {
-		return null ;
-	}
-
-	public void newConst(String name, int line, Object value) {
-
-	}
-
-	public void newVar(String name, int line, boolean isGlobal) {
-
-	}
-
-	public void newArray(String name, int line, boolean isGlobal) {
-
-	}
-
-	public void funcType (Struct type, String name, int line) {
-
-	}
-
-	public void endFunc() {
-
-	}
-
-	public void funcStart() {
-
-	}
-
-	public void newEqualExpression(Obj designator, Struct expression, int line) {
-
-	}
-
-	public Obj getObj(String ident) {
-		Obj res = null;
-		return res;
-	}
-
-	public void detection(String ident, int line) {
-            
-	}
-
-	public void newArray(String ident, int line) {
-
-	}
-
-	public void checkInt(Struct type, int line) {
-
-	}
-
-	public Struct checkMinus(Struct term, int line) {
-		Struct res = null;
-		return res;
-	}
-
-	public Struct addOp(Struct first, Struct second, Integer operation, int line) {
-		Struct res = null;
-		return res;
-	}
-
-	public Struct mulOp(Struct first, Struct second, Integer operation, int line) {
-		Struct res = null;
-		return res;
-	}
-
-	public Struct newDesFactor (Obj designator) {
-		Struct res = null;
-		return res;
-	}
-
-	public Struct newFuncFactor(Obj designator, int line, int v) {
-		Struct res = null;
-		return res;
-	}
-
-	public Struct newExprFactor(Struct expr) {
-		Struct res = null;
-		return res;
-	}
-
-	public Struct newArrFactor(Struct first, Struct second, int line) {
-		Struct res = null;
-		return res;
-	}
-
-	public void increment (Obj designator, int line) {
-
-	}
-
-	public void decrement (Obj designator, int line) {
-
-	}
-
-	public void returnMatched(Struct type, int line) {
-
-	}
-
-	public void readMatched(Obj designator, int line) {
-
-	}
-
-	public void printMatched (Struct type, Integer length, int line) {
-
-	}
-
-	public void newFunction(Obj designator, int line) {
-
-	}
+    public Integer printCallCount = 0;
+    private Boolean mainIsDefined = false;
+    public Map<SymbolOccurence, Integer> mapOfEnumerations = new EnumMap<SymbolOccurence, Integer>(SymbolOccurence.class);
+    public Scope currentScope;
+    public Obj currentProgram;
+    public Scope universeScope;
+    public Logger log = Logger.getLogger(getClass());
+    
+    public void reportInfo(String msg, int line) {
+        System.out.println(msg + " " + line);
+        log.info(msg.toString());
+    }
+    
+    public void reportInfo(String msg) {
+        System.out.println(msg);
+        log.info(msg.toString());
+    }
+    
+    public void reportError(String message, Object info) {
+      System.err.print(message);
+      System.err.flush();
+      if (info instanceof Symbol)
+            System.err.println("Error : on line " + ((Symbol)info).left);
+      else System.err.println("");
+    }
+    
+    public void reportError(String message) {
+      System.err.println(message);
+      System.err.flush();
+    }
+    
+    public void unrecoveredSyntaxError(Symbol curToken) throws java.lang.Exception {
+        reportFatalError("Error : parsing has to be stopped", curToken);
+    }
+    
+    public void reportFatalError(String message, Object info) throws java.lang.Exception {
+      reportError(message, info);
+    }
+    
+    public void startProgram(String progName) {
+        reportInfo("Program named \""+progName+"\" STARTED.");
+    }
+    
+    public void endProgram() {
+        if(!mainIsDefined) {
+            String tmp = "Error: Main function not found."; 
+            reportError(tmp);
+            log.info(tmp);
+        }	
+//        Tab.chainLocalSymbols(currentProgram);
+        Tab.closeScope();
+    }
+    
+    public void increment(SymbolOccurence item) {
+        mapOfEnumerations.putIfAbsent(item, 0);
+        int tmp = mapOfEnumerations.get(item);
+        mapOfEnumerations.put(item, tmp+1);
+    }
 }
---
+++ src/utils/SymbolOccurence.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author obabovic
+ */
+public enum SymbolOccurence {
+    GLOBAL_CONST_DEFINITIONS,
+    GLOBAL_VAR_DEFINITIONS,
+    GLOBAL_ARRAY_DEFINITIONS,
+    
+    MAIN_VAR_DEFINITIONS,
+    MAIN_METHOD_CALLS,
+    
+    INNERCLASS_DEFINITIONS,
+    INNERCLASS_GLOBAL_AND_STATIC_FUNCTION_DEFINITIONS,
+    INNERCLASS_METHOD_DEFINITIONS,
+    INNERCLASS_FIELD_DECLARATIONS,
+    
+    BLOCK_STATEMENTS,
+    FORMAL_ARGUMENTS_DECLARATIONS
+}
