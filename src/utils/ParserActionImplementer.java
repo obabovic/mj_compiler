0c293cc85eca0ea4a645183e0655d9cbf27168c2
Ognjen Babovic <obabovic@MEGAZORD.local>
2016-12-24 19:00:36 +0100

    Add addConstant functionality

--- src/utils/ParserActionImplementer.java
+++ src/utils/ParserActionImplementer.java
@@ -1,5 +1,6 @@
 package utils;
 
+import JFlex.sym;
 import java_cup.runtime.*;
 import rs.etf.pp1.symboltable.Tab;
 import rs.etf.pp1.symboltable.concepts.*;
@@ -15,9 +16,22 @@ public class ParserActionImplementer {
     public Integer printCallCount = 0;
     private Boolean mainIsDefined = false;
     public Map<SymbolOccurence, Integer> mapOfEnumerations = new EnumMap<SymbolOccurence, Integer>(SymbolOccurence.class);
-    public Scope currentScope;
+    public Scope globalScope;
+    
     public Obj currentProgram;
-    public Scope universeScope;
+    public Scope currentScope;
+    public Struct currentVarType;
+    public Struct currentConstType;
+    public Struct currentMethodType;
+    
+    public static final int NUMBER = 25;
+    public static final int CHAR = 23;
+    public static final int BOOL = 24;
+    
+    public static final Struct intType = new Struct(NUMBER, Tab.intType);
+    public static final Struct charType = new Struct(CHAR, Tab.charType);
+    public static final Struct boolType = new Struct(BOOL);
+    
     public Logger log = Logger.getLogger(getClass());
     
     public void reportInfo(String msg, int line) {
@@ -34,7 +48,7 @@ public class ParserActionImplementer {
       System.err.print(message);
       System.err.flush();
       if (info instanceof Symbol)
-            System.err.println("Error : on line " + ((Symbol)info).left);
+            System.err.println("Error! Line " + ((Symbol)info).left);
       else System.err.println("");
     }
     
@@ -44,7 +58,7 @@ public class ParserActionImplementer {
     }
     
     public void unrecoveredSyntaxError(Symbol curToken) throws java.lang.Exception {
-        reportFatalError("Error : parsing has to be stopped", curToken);
+        reportFatalError("Error! Parsing has to be stopped.", curToken);
     }
     
     public void reportFatalError(String message, Object info) throws java.lang.Exception {
@@ -53,11 +67,20 @@ public class ParserActionImplementer {
     
     public void startProgram(String progName) {
         reportInfo("Program named \""+progName+"\" STARTED.");
+        Tab.insert(Obj.Type, "int", intType);
+        Tab.insert(Obj.Type, "char", charType);
+        Tab.insert(Obj.Type, "bool", boolType);
+        
+        
+        globalScope = Tab.currentScope();
+        currentProgram = Tab.insert(Obj.Prog, progName, Tab.noType);
+        Tab.openScope();
+        currentScope = Tab.currentScope();
     }
     
     public void endProgram() {
         if(!mainIsDefined) {
-            String tmp = "Error: Main function not found."; 
+            String tmp = "Error! Main function has not been found."; 
             reportError(tmp);
             log.info(tmp);
         }	
@@ -69,5 +92,60 @@ public class ParserActionImplementer {
         mapOfEnumerations.putIfAbsent(item, 0);
         int tmp = mapOfEnumerations.get(item);
         mapOfEnumerations.put(item, tmp+1);
+    }
+    
+    public Struct resolveType(String typeName, int line) {
+        Struct res = null;
+        Obj obj = Tab.find(typeName);
+        if((obj == Tab.noObj) || (obj.getKind() != Obj.Type)) 
+            reportError("Error! Undefined type \"" + typeName + "\" on line ", line);
+        res = obj.getType();
+        return res;
+    }
+    
+    public void addConst(String constName, Object constValue, int line) {
+        if(Tab.currentScope().findSymbol(constName) != null) {
+            reportError("Error! Constant named \"" + constName + "\" has already been declared. Line " , line);
+        } else {
+            int address = 0;
+            int constKind = currentConstType.getKind();
+            
+            if(((constKind==1)&&(!((constValue instanceof Integer)||(constValue instanceof Boolean))))||((constKind==2)&&(!(constValue instanceof Character)))) {
+                reportError("Error! Constant named \"" + constName + "\" has different type and value. Line " , line);
+            } else {
+                if(constValue instanceof Integer) {
+                    address = (Integer) constValue;
+                } else if (constValue instanceof Character) {
+                    address = (Character) constValue;
+                } else if (constValue instanceof Boolean) {
+                    address = ((Boolean)constValue) == null ? 0 : 1;
+                }
+                Tab.insert(Obj.Con, constName, currentConstType).setAdr(address);
+                increment(SymbolOccurence.GLOBAL_CONST_DEFINITIONS);
+            }
+        }
+        
+    }
+    
+    public void addVar(String name, int line,boolean varIsGlobal) {
+        if(Tab.currentScope().findSymbol(name) != null)
+                reportError("Error! Variable \"" + name + "\" has already been declared. Line " , line);
+        else {
+            Obj temp = Tab.insert(Obj.Var, name, currentVarType);
+                
+        }
+    }
+    
+    public void addMethod(String methodName, Struct methodType, int line) {
+        currentMethodType = methodType;
+        if("main".equals(methodName)) {
+            mainIsDefined = true;
+        }
+        
+        if(currentMethodType != null) {
+            
+        } else {
+        
+        }
     }
 }
